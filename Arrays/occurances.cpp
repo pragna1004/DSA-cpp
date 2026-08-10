be39bdc0b52b523a7a5bd4dffdf17e420e858fee
unknown <saipragna410@gmail.com>
2026-08-10 12:31:10 +0530

    Counting the max occured number's count

---
+++ Arrays/occurances.cpp
@@ -0,0 +1,23 @@
+#include <iostream>
+#include <unordered_set>
+using namespace std;
+int main(){
+    int arr[]={1,2,1,1,2,1,3};
+    int n=sizeof(arr)/sizeof(arr[0]);
+    unordered_set<int> visited;
+    for(int i=0;i<n;i++){
+        if(visited.find(arr[i])==visited.end()){
+            int count=0;
+            for(int j=0;j<n;j++){
+               if(arr[i]==arr[j]){
+                count++;
+               }
+            }
+            visited.insert(arr[i]);
+            if(count>n/2){
+             cout<<count;
+             return 0;
+            }
+        }
+    }
+}
