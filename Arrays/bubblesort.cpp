457485fa5502969bf4932be107acd6f715caf2d3
unknown <saipragna410@gmail.com>
2026-08-11 20:16:16 +0530

    Bubble sort algo

---
+++ Arrays/bubblesort.cpp
@@ -0,0 +1,19 @@
+#include <iostream>
+using namespace std;
+int main(){
+    int arr[]={3,4,2,5,6};
+    int n=sizeof(arr)/sizeof(arr[0]);
+    for(int i=0;i<n;i++){
+        for(int j=0;j<n-i-1;j++){
+            if(arr[j]>arr[j+1]){
+                int temp=arr[j];
+                arr[j]=arr[j+1];
+                arr[j+1]=temp;
+            }
+            }
+}
+for(int i=0;i<n;i++){
+    cout<<arr[i]<<" ";
+}
+return 0;
+}
